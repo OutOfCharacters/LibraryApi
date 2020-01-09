39e1165383147cb0ad20b514344b74d68d649cf0
Jonathan <walrus365@sbcglobal.net>
2020-01-09 10:13:26 -0500

    Get Document Added

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -16,6 +16,46 @@ namespace LibraryApi.Controllers
             Context = context;
         }
 
+        //Get /books/{id}
+        [HttpGet("/books/{id:int}")]
+        public async Task<IActionResult> GetABook(int id)
+        {
+            //Single or default returns either one value, or default(null) or throws an exception if multiple objects are to be returned 
+            var result = await Context.Books
+                .Select(b => new GetBookResponseDocument
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Genre = b.Genre
+                }).SingleOrDefaultAsync(b => b.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("That book isn't in our library");
+            }
+            else
+            {
+                return Ok(result);
+            }
+        }
+
+        [HttpPost("/books")]
+        public async Task<IActionResult> AddABook()
+        {
+            // Validate the thingy
+            // Not? -> Return a 400 status (Bad Request)
+            // Is Valid?
+            //  Add it to the database
+            //  Return a 201 (created) status code.
+            //  Add a location header to the response that has the URL for the new baby resource.
+            //  And, if you are nice, send them a copy of the new resource as well.
+
+
+
+            return BadRequest();
+        }
+
         [HttpGet("/books")]
         public async Task<IActionResult> GetAllBooks([FromQuery] string genre = "all")
         {
@@ -32,6 +72,7 @@ namespace LibraryApi.Controllers
                 allBooks = allBooks.Where(b => b.Genre == genre);
             }
             response.Books = await allBooks.ToListAsync();
+            response.GenreFilter = genre;
 
             return Ok(response);
         }
---
+++ Models/GetBookResponseDocument.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryApi.Models
+{
+    public class GetBookResponseDocument
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+    }
+}
